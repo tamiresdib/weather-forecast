709eadefc2b6b258eca56ab572e0eb51f8d4cece
Tamires Dib <admin@Admins-MacBook-Air.local>
2026-07-19 01:08:31 -0300

    chore(project): create vite react app

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-slate-950 text-white">
+      <h1 className="text-3xl font-bold">Weather Forecast</h1>
+    </main>
+  );
+}
+
+export default App;
